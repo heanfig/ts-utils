362ce179b2466bbadc24122ca998805973145181
Herman Andres <hernan.figueroa@novatec.com.co>
2022-02-27 23:54:10 -0500

    feat(general): Fix dependencies error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Memoize } from '@heanfig/ts-utils';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ts-utils-demo';
 
-  public demo(){
-
+  @Memoize()
+  public getValue(value: number){
+    return value ** 45;
   }
+
 }
